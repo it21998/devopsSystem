202dcd593a571c06e98a82a0fb4e3898a173577f
it21998 <79991769+it21998@users.noreply.github.com>
2022-06-05 01:10:57 +0300

    Update settings.py

--- student_management/student_management/settings.py
+++ student_management/student_management/settings.py
@@ -45,8 +45,7 @@ MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,"media")
 
 
-STATIC_URL='/static/'
-STATIC_ROOT=os.path.join(BASE_DIR,"static")
+
 
 
 # Application definition
@@ -139,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 AUTH_USER_MODEL="student_management_app.CustomUser"
 AUTHENTICATION_BACKENDS=['student_management_app.EmailBackEnd.EmailBackEnd']
 
